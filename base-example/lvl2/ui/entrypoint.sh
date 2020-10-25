b7fa9d1b29280e07a4b294ac2786421de2b80008
davidlennick <david.lennick@gmail.com>
2020-10-25 05:38:44 -0400

    updaed to add killswitch

--- base-example/lvl2/ui/entrypoint.sh
+++ base-example/lvl2/ui/entrypoint.sh
@@ -2,6 +2,18 @@
 
 API_URI="http://$API_ADDR:$API_PORT"
 
+attempt_counter=0
+max_attempts=10
+
+until $(curl --output /dev/null --silent --head --fail $API_URI); do
+     if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+    printf '.'
+    sleep 3
+done
+
 curl -X POST -d task='test task' -d 'is_complete=false' $API_URI/task
 SERVER_SIDE_GET_BASE=$(curl $API_URI)
 SERVER_SIDE_GET_TASKS=$(curl $API_URI/tasks)
